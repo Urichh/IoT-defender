f69d883feaa46611c36fcb28c9c919930dd5eb35
Urichh <urban.dopudja@gmail.com>
2023-10-28 01:26:16 +0200

    osnovni discover dila, zdej optimizacija

--- IoT defender/IoT defender/Form1.Designer.cs
+++ IoT defender/IoT defender/Form1.Designer.cs
@@ -34,6 +34,12 @@ namespace IoT_defender
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ip = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hostname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
@@ -53,9 +59,9 @@ namespace IoT_defender
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.Size = new System.Drawing.Size(87, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Subnet/IP range:";
+            this.label1.Text = "subnet/IP range:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
@@ -64,20 +70,57 @@ namespace IoT_defender
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(69, 27);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Scan";
+            this.button1.Text = "scan";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ip,
+            this.hostname,
+            this.status});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(707, 408);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // ip
+            // 
+            this.ip.HeaderText = "IP address";
+            this.ip.Name = "ip";
+            // 
+            // hostname
+            // 
+            this.hostname.HeaderText = "Host name";
+            this.hostname.Name = "hostname";
+            // 
+            // status
+            // 
+            this.status.HeaderText = "Status";
+            this.status.Name = "status";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "IoT Defender";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,6 +132,11 @@ namespace IoT_defender
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ip;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hostname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn status;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- IoT defender/IoT defender/Form1.cs
+++ IoT defender/IoT defender/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 
 namespace IoT_defender
 {
@@ -25,6 +29,51 @@ namespace IoT_defender
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(100);
+            Ping ping;
+            IPAddress addr;
+            PingReply pingReply;
+            IPHostEntry host;
+            string name;
+
+            Parallel.For(0, 254, (i, loopState) =>
+            {
+                    ping = new Ping();
+                    pingReply = ping.Send(textBox1.Text + i.ToString());
+
+                this.BeginInvoke((Action)delegate ()
+                {
+                    if (pingReply.Status == IPStatus.Success)
+                    {
+                        try
+                        {
+                            addr = IPAddress.Parse(textBox1.Text + i.ToString());
+                            host = Dns.GetHostEntry(addr);
+                            name = host.HostName;
+
+                            dataGridView1.Rows.Add();
+                            int nRowIndex = dataGridView1.Rows.Count - 1;
+                            dataGridView1.Rows[nRowIndex].Cells[0].Value = textBox1.Text + i.ToString();
+                            dataGridView1.Rows[nRowIndex].Cells[1].Value = name;
+                            dataGridView1.Rows[nRowIndex].Cells[2].Value = "Active";
+                        }
+                        catch (Exception ex)
+                        {
+                            name = "?";
+                        }
+                    }
+                });
+            });
+            MessageBox.Show("Scan completed");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
         }
     }
 }
