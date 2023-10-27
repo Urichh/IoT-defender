084c224b8dea9c9bbf3c89f77374fa01518d2283
Urichh <urban.dopudja@gmail.com>
2023-10-28 01:51:05 +0200

    mal code refactoringa narjenga, osnovna funkcionalnost uspostavljena. Željeni razne izboljšave/popravke, katere bom (prolly) zapisau u branch merge

--- IoT defender/IoT defender/Form1.cs
+++ IoT defender/IoT defender/Form1.cs
@@ -7,10 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace IoT_defender
 {
@@ -30,50 +30,51 @@ namespace IoT_defender
         {
 
         }
-
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-            Thread.Sleep(100);
-            Ping ping;
-            IPAddress addr;
-            PingReply pingReply;
-            IPHostEntry host;
-            string name;
-
-            Parallel.For(0, 254, (i, loopState) =>
-            {
-                    ping = new Ping();
-                    pingReply = ping.Send(textBox1.Text + i.ToString());
-
-                this.BeginInvoke((Action)delegate ()
-                {
-                    if (pingReply.Status == IPStatus.Success)
-                    {
-                        try
-                        {
-                            addr = IPAddress.Parse(textBox1.Text + i.ToString());
-                            host = Dns.GetHostEntry(addr);
-                            name = host.HostName;
-
-                            dataGridView1.Rows.Add();
-                            int nRowIndex = dataGridView1.Rows.Count - 1;
-                            dataGridView1.Rows[nRowIndex].Cells[0].Value = textBox1.Text + i.ToString();
-                            dataGridView1.Rows[nRowIndex].Cells[1].Value = name;
-                            dataGridView1.Rows[nRowIndex].Cells[2].Value = "Active";
-                        }
-                        catch (Exception ex)
-                        {
-                            name = "?";
-                        }
-                    }
-                });
-            });
-            MessageBox.Show("Scan completed");
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            backgroundWorker1.RunWorkerAsync();
+            bw.RunWorkerAsync();
+        }
+        private void Scan(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(100);
+            string name = "";
+
+            IPAddress ip_address;
+
+            Ping curr;
+            PingReply reply;
+            IPHostEntry host;
+
+            for (int i = 0; i < 254; i++)
+            {
+                curr = new Ping();
+                reply = curr.Send(textBox1.Text + i.ToString());
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    try
+                    {
+                        ip_address = IPAddress.Parse(textBox1.Text + i.ToString());
+                        host = Dns.GetHostEntry(ip_address);
+                        name = host.HostName;
+
+                        this.BeginInvoke((Action)delegate ()
+                        {
+                            int n = dgv.Rows.Count;
+                            dgv.Rows.Add();
+                            
+                            dgv.Rows[n].Cells[0].Value = textBox1.Text + i.ToString();
+                            dgv.Rows[n].Cells[1].Value = name;
+                            dgv.Rows[n].Cells[2].Value = "Active";
+                        });
+                    }
+                    catch (Exception exception)
+                    {
+
+                    }
+                }
+            }
+            MessageBox.Show("Subnet scan completed");
         }
     }
 }
