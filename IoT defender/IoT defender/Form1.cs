76f748488dad3c8d231deb82385b38dbdd6fd081
Urichh <urban.dopudja@gmail.com>
2023-10-31 20:17:27 +0100

    reworked entire system for pinging to now (hopefully) incluee all possible subnets

--- IoT defender/IoT defender/Form1.cs
+++ IoT defender/IoT defender/Form1.cs
@@ -47,105 +47,60 @@ namespace IoT_defender
 
             string counter = "";
 
-            //legit se ful pahitri pinging naslovov če bereš text od labela sam enkrat pa storaš u string.
-            //(prej se je vsakič accessalu value pa je tu zgleda "pažrišnu")
             string currentAddr = input_ip.Text;
             string[] currentTemp = currentAddr.Split('/');
             string currentPing = currentTemp[0];
             int currentCIDR = Int32.Parse(currentTemp[1]);
 
-            currentTemp = currentPing.Split('.');
-            //fyi current parts == octets, sam nism se spovnu prej te beside
-            int[] currentParts = { Int32.Parse(currentTemp[0]), Int32.Parse(currentTemp[1]), Int32.Parse(currentTemp[2]), Int32.Parse(currentTemp[3]) };
+            IPAddress ip = IPAddress.Parse(currentPing);
+            byte[] ipBytes = ip.GetAddressBytes();
 
-            //debugging.......
-            Console.WriteLine(currentPing);
-            Console.WriteLine(currentCIDR);
+            uint mask = ~(uint.MaxValue >> currentCIDR);
+            byte[] maskBytes = BitConverter.GetBytes(mask);
 
-            for (int i = 0; i < currentParts.Length; i++)
-                Console.Write(currentParts[i] + ", ");
-            //................
-
-            string currentStartIP = "";
-            int currentLastOctet = -1;
-
-            int currentMaskedPart = 0;
-            if (currentCIDR > 0 && currentCIDR < 36)
+            if (BitConverter.IsLittleEndian)
             {
-                string maskedPart = "";
-                if (currentCIDR < 8)
-                {
-                    maskedPart = Convert.ToString(currentParts[0], 2).PadLeft(8, '0');
-                    maskedPart = maskedPart.Substring(0, currentCIDR % 8).PadRight(8, '0');
-                    currentMaskedPart = Convert.ToInt32(maskedPart, 2);
-                    currentStartIP = currentMaskedPart + ".0.0.0";
-                    currentLastOctet = 0;
-                }
-                else if (currentCIDR >= 8 && currentCIDR < 16)
-                {
-                    maskedPart = Convert.ToString(currentParts[1], 2).PadLeft(8, '0');
-                    maskedPart = maskedPart.Substring(0, currentCIDR % 8).PadRight(8, '0');
-                    currentMaskedPart = Convert.ToInt32(maskedPart, 2);
-                    currentStartIP = currentParts[0] + "." + currentMaskedPart + ".0.0";
-                    currentLastOctet = 1;
-                }
-                else if (currentCIDR >= 16 && currentCIDR < 24)
-                {
-                    maskedPart = Convert.ToString(currentParts[2], 2).PadLeft(8, '0');
-                    maskedPart = maskedPart.Substring(0, currentCIDR % 8).PadRight(8, '0');
-                    currentMaskedPart = Convert.ToInt32(maskedPart, 2);
-                    currentStartIP = currentParts[0] + "." + currentParts[1] + "." + currentMaskedPart + ".0";
-                    currentLastOctet = 2;
-                }
-                else
-                {
-                    maskedPart = Convert.ToString(currentParts[3], 2).PadLeft(8, '0');
-                    maskedPart = maskedPart.Substring(0, currentCIDR % 8).PadRight(8, '0');
-                    currentMaskedPart = Convert.ToInt32(maskedPart, 2);
-                    currentStartIP = currentParts[0] + "." + currentParts[1] + "." + currentParts[2] + "." + currentMaskedPart;
-                    currentLastOctet = 3;
-                }
-                Console.WriteLine("\nSTART IP: " + currentStartIP);
+                Array.Reverse(maskBytes);
             }
-            else
+
+            byte[] networkAddress = new byte[ipBytes.Length];
+            byte[] broadcastAddress = new byte[ipBytes.Length];
+
+            for (int i = 0; i < ipBytes.Length; i++)
             {
-                Console.WriteLine("ERROR: CIDR OUT OF BOUNDS");
+                networkAddress[i] = (byte)(ipBytes[i] & maskBytes[i]);
+                broadcastAddress[i] = (byte)(networkAddress[i] | ~maskBytes[i]);
             }
-            if(currentLastOctet == -1)
-                Console.WriteLine("ERROR: INVALID OCTET");
 
-            //todo: to gre u infinite loop u primeru invalid octeta
-            //nism sure glede pogoja tbh
-            //todo: fix tale da 254, nism zihr lih kaku bi moglu bit no
-            IPAddress convertanIP = IPAddress.Parse(currentStartIP);
-            uint prviIP = BitConverter.ToUInt32(convertanIP.GetAddressBytes(), 0);
-            IPAddress convertanZadnIP = IPAddress.Parse("255.255.255.255");
-            uint zadnIP = BitConverter.ToUInt32(convertanZadnIP.GetAddressBytes(), 0);
+            IPAddress prviIP = new IPAddress(networkAddress);
+            IPAddress zadnIP = new IPAddress(broadcastAddress);
 
-            //Array.Reverse(bytes);
-            for (uint i = prviIP + 1; i <= zadnIP; i++)
+            Console.WriteLine("Start IP: " + prviIP.ToString());
+            Console.WriteLine("End IP: " + zadnIP.ToString());
+
+
+            byte[] ipBytesIterated = networkAddress.ToArray();
+            while (!ipBytesIterated.SequenceEqual(broadcastAddress))
             {
-                byte[] bytes = BitConverter.GetBytes(i);
-
-
-                if (i != prviIP + 1)
-                {
-                    //Array.Reverse(bytes);
-                    Console.WriteLine("i indeed ni enaka prvi ip");
-                }
+                //ipBytesIterated = BitConverter.GetBytes(ipBytesIterated);
 
                 //if (BitConverter.IsLittleEndian)
-                    //Array.Reverse(bytes);
+                //{
+                    //Array.Reverse(ipBytesIterated);
+                //}
+
+                //if (BitConverter.IsLittleEndian)
+                //Array.Reverse(bytes);
 
                 //currentPing = currentParts[0] + "." + currentParts[1] + "." + currentParts[2] + "." + i;
 
-                IPAddress currentIP = new IPAddress(bytes);
+                IPAddress currentIP = new IPAddress(ipBytesIterated);
 
                 curr = new Ping();
                 Console.WriteLine("Current IP: " + currentIP);
-                Console.WriteLine("i: " + i);
-                for (int k = 0; k < bytes.Length; k++)
-                    Console.WriteLine(k + ": " + bytes[k]);
+                //Console.WriteLine("i: " + i);
+                //for (int k = 0; k < bytes.Length; k++)
+                    //Console.WriteLine(k + ": " + bytes[k]);
                 reply = curr.Send(currentIP);
 
                 
@@ -182,11 +137,25 @@ namespace IoT_defender
 
                     }
                 }
+
+                IncrementIPAddressBytes(ref ipBytesIterated);
+
                 //Array.Reverse(bytes);
             }
             MessageBox.Show("Subnet scan completed");
         }
+        private void IncrementIPAddressBytes(ref byte[] ipBytes)
+        {
+            for (int i = ipBytes.Length - 1; i >= 0; i--)
+            {
+                ipBytes[i]++;
 
+                if (ipBytes[i] != 0) // No overflow, exit the loop
+                {
+                    break;
+                }
+            }
+        }
         private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
